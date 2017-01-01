46e7f383f307275bee09cd2a5dac5e2875e78938
Nikita Smolyakov <smolyakov.na@gmail.com>
2017-01-01 23:09:40 +0300

    Add all remaining group-side behaviours; hopefully it should work, still no way to check though

--- src/main/java/GroupAgent.java
+++ src/main/java/GroupAgent.java
@@ -1,5 +1,8 @@
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.SequentialBehaviour;
+import jade.core.behaviours.SimpleBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
@@ -17,7 +20,12 @@ public class GroupAgent extends Agent{
     private String productName;
     private String shopAddress;
     private int condition;
-    private Map<String, Integer> buyers;
+    // Address and desired quantity of products
+    private Map<String, Integer> buyersDesires;
+    private Map<String, Boolean> buyersReadiness;
+
+    private boolean conditionFulfilled;
+    private MessageTemplate template;
 
     // Gets buyer and their desired quantity of the product, enlists them and replies
     private class EnlistBuyers extends CyclicBehaviour {
@@ -26,9 +34,9 @@ public class GroupAgent extends Agent{
             ACLMessage message = myAgent.receive();
 
             if (message != null) {
-                String buyer = message.getContent().split(" ")[0];
-                int quantity = Integer.valueOf(message.getContent().split(" ")[1]);
-                buyers.put(buyer, quantity);
+                AID buyer = message.getSender();
+                int quantity = Integer.valueOf(message.getContent());
+                buyersDesires.put(buyer.getName(), quantity);
 
                 ACLMessage reply = message.createReply();
                 reply.setPerformative(ACLMessage.INFORM);
@@ -40,13 +48,100 @@ public class GroupAgent extends Agent{
         }
     }
 
+    // If condition is fulfilled checks readiness of all current buyers and then sends them purchase confirmation
+    private class Purchase extends SequentialBehaviour {
+        public int onEnd() {
+            reset();
+            myAgent.addBehaviour(this);
+            return super.onEnd();
+        }
+    }
+    private class CheckEnlistedIfConditionReached extends SimpleBehaviour {
+        private boolean finished = false;
+
+        public void action() {
+            int sum = 0;
+            for (int quantity: buyersDesires.values()) {
+                sum += quantity;
+            }
+            if (sum < condition) {
+                finished = true;
+                conditionFulfilled = false;
+                return;
+            }
+            conditionFulfilled = true;
+
+            for (String buyerAddress: buyersDesires.keySet()) {
+                ACLMessage message = new ACLMessage(ACLMessage.QUERY_IF);
+                AID buyer = new AID(buyerAddress);
+                message.addReceiver(buyer);
+                message.setContent("ready_for_purchase?");
+                message.setConversationId("group_purchase");
+                message.setReplyWith(String.valueOf(System.currentTimeMillis()));
+                myAgent.send(message);
+                template = MessageTemplate.and(MessageTemplate.MatchConversationId("group_purchase"), MessageTemplate.MatchInReplyTo(message.getReplyWith()));
+            }
+            buyersReadiness = new HashMap<String, Boolean>();
+            finished = true;
+        }
+
+        public boolean done(){
+            return finished;
+        }
+    }
+    private class GetRepliesAndPurchaseIfEveryoneReady extends SimpleBehaviour {
+        private boolean finished = false;
+        public void action() {
+            if (!conditionFulfilled) {
+                finished = true;
+                return;
+            }
+
+            ACLMessage reply = myAgent.receive(template);
+            if (reply != null) {
+                if (reply.getContent().equals("ready")) {
+                    buyersReadiness.put(reply.getSender().getName(), true);
+
+                    for (boolean readiness: buyersReadiness.values()) {
+                        if (!readiness) {
+                            finished = true;
+                            return;
+                        }
+                    }
+
+                    for (String buyerAddress: buyersReadiness.keySet()) {
+                        ACLMessage message = new ACLMessage(ACLMessage.INFORM);
+                        AID buyer = new AID(buyerAddress);
+                        message.addReceiver(buyer);
+                        message.setContent(productName);
+                        message.setConversationId("group_purchase");
+                        message.setReplyWith(String.valueOf(System.currentTimeMillis()));
+                        myAgent.send(message);
+                    }
+
+                    buyersDesires.clear();
+                    finished = true;
+                }
+            }
+        }
+
+        public boolean done() {
+            return finished;
+        }
+    }
+
     protected void setup() {
         getArgs();
         register();
 
-        buyers = new HashMap<String, Integer>();
+        buyersDesires = new HashMap<String, Integer>();
 
         addBehaviour(new EnlistBuyers());
+
+        SequentialBehaviour purchase = new Purchase();
+        purchase.addSubBehaviour(new CheckEnlistedIfConditionReached());
+        purchase.addSubBehaviour(new GetRepliesAndPurchaseIfEveryoneReady());
+        addBehaviour(purchase);
 
         logAppearing();
     }
