e7fbd649a2746180fdea5b1a8e23a70e89a91ddb
Nikita Smolyakov <smolyakov.na@gmail.com>
2017-01-01 18:21:36 +0300

    Add comments on yellow pages service naming

--- src/main/java/YellowPagesParser.java
+++ src/main/java/YellowPagesParser.java
@@ -2,6 +2,10 @@
  * Created by Nikita on 31.12.2016.
  */
 public class YellowPagesParser {
+    // Shop product service info in yellow pages:
+    //  service type: "Shop_product"
+    //  service name: "<product name> <price> <wholesale price> <condition>"
+
     public static String getShopProductName(String shopProductInfo){
         return shopProductInfo.split(" ")[0];
     }
@@ -17,6 +21,10 @@ public class YellowPagesParser {
     public static int getShopProductCondition(String shopProductInfo){
         return Integer.parseInt(shopProductInfo.split(" ")[3]);
     }
+
+    // Group service info in yellow pages:
+    //  service type: "Group"
+    //  service name: "<product_name> <shop_name>"
 
     public static String getGroupProductName(String groupInfo){
         return groupInfo.split(" ")[0];
