88091438385353d26884f07147528f5eb9d4e036
Nikita Smolyakov <smolyakov.na@gmail.com>
2017-01-05 16:01:57 +0300

    Remove useless comments

--- src/main/java/BootstrapAgent.java
+++ src/main/java/BootstrapAgent.java
@@ -21,33 +21,6 @@ public class BootstrapAgent extends Agent{
         ContainerController containerController = getContainerController();
         AgentController agentController;
         try {
-//            Product potato = new Product("potato", 5, 3, 10);
-//            Product youngPotato = new Product("young_potato",  10, 6, 10);
-//            Product tomato = new Product("tomato", 20, 12, 30);
-//            Product maize = new Product("maize",  8, 5, 20);
-//            Product cucumber = new Product("cucumber", 15, 10, 15);
-//
-//            List<Product> potatoCountryStock = new LinkedList<Product>();
-//            potatoCountryStock.add(potato);
-//            potatoCountryStock.add(youngPotato);
-//            Object[] potatoCountryArgs = {potatoCountryStock};
-//            agentController = containerController.createNewAgent("The Potato Country", "ShopAgent", potatoCountryArgs);
-//            agentController.start();
-//
-//            List<Product> royalStock = new LinkedList<Product>();
-//            royalStock.add(tomato);
-//            royalStock.add(cucumber);
-//            Object[] royalArgs = {royalStock};
-//            agentController = containerController.createNewAgent("Royal", "ShopAgent", royalArgs);
-//            agentController.start();
-//
-//            List<Product> effgStock = new LinkedList<Product>();
-//            effgStock.add(potato);
-//            effgStock.add(maize);
-//            Object[] effgArgs = {effgStock};
-//            agentController = containerController.createNewAgent("Ed's Friendly Farming Goods", "ShopAgent", effgArgs);
-//            agentController.start();
-
             // Name must not have spaces for parser to work!
             Product cheeseHead = new Product("Cheese_head", 30, 15, 15);
 
@@ -66,22 +39,6 @@ public class BootstrapAgent extends Agent{
         ContainerController containerController = getContainerController();
         AgentController agentController;
         try {
-            /*int mikeMoney = 100500;
-            Map<String, Integer> mikeWish = new HashMap<String, Integer>();
-            mikeWish.put("potato", 5);
-            mikeWish.put("tomato", 2);
-            mikeWish.put("maize", 1);
-            Object[] mikeArgs = {mikeMoney, mikeWish};
-            agentController = containerController.createNewAgent("Mike", "BuyerAgent", mikeArgs);
-            agentController.start();
-
-            int edwardMoney = 9000;
-            Map<String, Integer> edwardWish = new HashMap<String, Integer>();
-            edwardWish.put("coconut", 1);
-            Object[] edwardArgs = {edwardMoney, edwardWish};
-            agentController = containerController.createNewAgent("Edward", "BuyerAgent", edwardArgs);
-            agentController.start();*/
-
             for (int i = 0; i < 5; i++) {
                 int money = 1000 + i * 100;
                 Map<String, Integer> wish = new HashMap<String, Integer>();
