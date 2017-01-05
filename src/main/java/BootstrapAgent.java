2fc64342cae6dde2eca9dc0639e14294ed7c0642
Nikita Smolyakov <smolyakov.na@gmail.com>
2017-01-05 17:38:50 +0300

    Add more shops and buyers

--- src/main/java/BootstrapAgent.java
+++ src/main/java/BootstrapAgent.java
@@ -23,12 +23,19 @@ public class BootstrapAgent extends Agent{
         try {
             // Name must not have spaces for parser to work!
             Product cheeseHead = new Product("Cheese_head", 30, 15, 15);
+            Product bread = new Product("Bread", 20, 10, 10);
 
             List<Product> cheeseShopStock = new LinkedList<Product>();
             cheeseShopStock.add(cheeseHead);
             Object[] cheeseShopArgs = {cheeseShopStock};
             // Name must not have spaces for parser to work!
             agentController = containerController.createNewAgent("Cheese_shop", "ShopAgent", cheeseShopArgs);
+            agentController.start();
+
+            List<Product> breadShopStock = new LinkedList<Product>();
+            breadShopStock.add(bread);
+            Object[] breadShopArgs = {breadShopStock};
+            agentController = containerController.createNewAgent("Bread_shop", "ShopAgent", breadShopArgs);
             agentController.start();
         }
         catch (StaleProxyException spe) {
@@ -45,6 +52,27 @@ public class BootstrapAgent extends Agent{
                 wish.put("Cheese_head", 1 + i);
                 Object[] buyerArgs = {money, wish};
                 agentController = containerController.createNewAgent("Cheese buyer " + i, "BuyerAgent", buyerArgs);
+                agentController.start();
+                doWait(1000);
+            }
+
+            for (int i = 0; i < 5; i++) {
+                int money = 1000 + i * 100;
+                Map<String, Integer> wish = new HashMap<String, Integer>();
+                wish.put("Bread", 1 + i);
+                Object[] buyerArgs = {money, wish};
+                agentController = containerController.createNewAgent("Bread buyer " + i, "BuyerAgent", buyerArgs);
+                agentController.start();
+                doWait(1000);
+            }
+
+            for (int i = 0; i < 5; i++) {
+                int money = 1000 + i * 100;
+                Map<String, Integer> wish = new HashMap<String, Integer>();
+                wish.put("Bread", 1 + i);
+                wish.put("Cheese_head", 1 + i);
+                Object[] buyerArgs = {money, wish};
+                agentController = containerController.createNewAgent("Bread and cheese buyer " + i, "BuyerAgent", buyerArgs);
                 agentController.start();
                 doWait(1000);
             }
