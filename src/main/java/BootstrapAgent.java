20c7303fe3e2b7706ff4202d8ce8a029576cba27
Nikita Smolyakov <smolyakov.na@gmail.com>
2017-01-14 18:06:36 +0300

    Add demo for agents switching to different group

--- src/main/java/BootstrapAgent.java
+++ src/main/java/BootstrapAgent.java
@@ -12,8 +12,9 @@ import java.util.Map;
 
 public class BootstrapAgent extends Agent{
     protected void setup() {
-        bootstrapShops();
-        bootstrapAgents();
+//        bootstrapShops();
+//        bootstrapAgents();
+        bootstrapChangeGroupDemo();
         doDelete();
     }
 
@@ -73,6 +74,52 @@ public class BootstrapAgent extends Agent{
                 wish.put("Cheese_head", 1 + i);
                 Object[] buyerArgs = {money, wish};
                 agentController = containerController.createNewAgent("Bread and cheese buyer " + i, "BuyerAgent", buyerArgs);
+                agentController.start();
+                doWait(1000);
+            }
+        }
+        catch (StaleProxyException spe) {
+            spe.printStackTrace();
+        }
+    }
+    private void bootstrapChangeGroupDemo() {
+        ContainerController containerController = getContainerController();
+        AgentController agentController;
+
+        try {
+            Product expensivePotato = new Product("Potato", 10, 7, 10);
+
+            List<Product> expensivePotatoShopStock = new LinkedList<Product>();
+            expensivePotatoShopStock.add(expensivePotato);
+            Object[] expensivePotatoShopArgs = {expensivePotatoShopStock};
+            agentController = containerController.createNewAgent("Expensive_potato_shop", "ShopAgent", expensivePotatoShopArgs);
+            agentController.start();
+
+
+            for (int i = 0; i < 3; i++) {
+                int money = 1000 + i * 10;
+                Map<String, Integer> wish = new HashMap<String, Integer>();
+                wish.put("Potato", 2);
+                Object[] buyerArgs = {money, wish};
+                agentController = containerController.createNewAgent("Potato_buyer_" + i, "BuyerAgent", buyerArgs);
+                agentController.start();
+                doWait(1000);
+            }
+
+            Product cheapPotato = new Product("Potato", 9, 5, 10);
+
+            List<Product> cheapPotatoShopStock = new LinkedList<Product>();
+            cheapPotatoShopStock.add(cheapPotato);
+            Object[] cheapPotatoShopArgs = {cheapPotatoShopStock};
+            agentController = containerController.createNewAgent("Cheap_potato_shop", "ShopAgent", cheapPotatoShopArgs);
+            agentController.start();
+
+            for (int i = 3; i < 5; i++) {
+                int money = 1000 + i * 100;
+                Map<String, Integer> wish = new HashMap<String, Integer>();
+                wish.put("Potato", 2);
+                Object[] buyerArgs = {money, wish};
+                agentController = containerController.createNewAgent("Potato_buyer_" + i, "BuyerAgent", buyerArgs);
                 agentController.start();
                 doWait(1000);
             }
